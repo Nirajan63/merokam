82518a4d1c670d12cdafd827de7867491b89fdca
Nirajan63 <nirajansamir222@gmail.com>
2026-05-17 11:34:25 +0545

    route sikeko

--- app.py
+++ app.py
@@ -1,11 +1,45 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def home():
-    return "Hello, World! This is my first Flask website."
+def main_home():
+    return """
+        <h1>Kam ki baat</h1>
+        <a href="/youtube">Go to YouTube</a><br>
+        <a href="/home">Go to Home Page</a><br>
+        <a href="/about">Go to About Page</a><br>
+        <a href="/login">Go to Login Page</a><br>
+        <a href="/contact">Go to Contact Page</a>
+    """
+
+
+@app.route("/youtube")
+def youtube():
+    return """
+        <a href="https://www.youtube.com/" target="_blank">Jau ta YouTube</a>
+    """
+
+
+@app.route("/home")
+def home_page():
+    return render_template("home.html")
+
+
+@app.route("/contact")
+def contact_page():
+    return render_template("contact.html")
+
+
+@app.route("/login")
+def login_page():
+    return render_template("login.html")
+
+
+@app.route("/about")
+def about_page():
+    return render_template("about.html")
 
 
 if __name__ == "__main__":
